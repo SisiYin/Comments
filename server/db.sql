4a27be95a7eb396e0f4a8c424b67ca5fc47ed811
SisiYin <t3chyi00@students.oamk.fi>
2024-04-20 13:30:35 +0300

    date

--- server/db.sql
+++ server/db.sql
@@ -1,11 +1,17 @@
-create datebase comments;
-use comments;
 
-CREATE TABLE comments (
-    id int PRIMARY KEY,
-    comment VARCHAR(800) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+
+INSERT INTO comment (comment_id, account_id, post_id, comment ,date) 
+VALUES 
+    (1, 1,1, 'good' ,'2024-03-20 10:00:00'::timestamp with time zone),
+    (2, 1, 2 , 'easy' ,'2024-03-21 11:00:00'::timestamp with time zone),
+    (3, 3, 5 , 'sweet' ,'2024-03-25 12:00:00'::timestamp with time zone),
+    (4, 2, 1 , 'very good' ,'2024-03-25 13:00:00'::timestamp with time zone),
+    (5, 3, 3 , 'very spicy' ,'2024-03-31 11:00:00'::timestamp with time zone),
+    (6, 2, 2 , 'very good' ,'2024-04-15 13:00:00'::timestamp with time zone),
+    (7, 4, 5 , 'good' ,'2024-04-19 13:00:00'::timestamp with time zone),
+    (8, 4, 4 , 'good' ,'2024-04-19 17:00:00'::timestamp with time zone),
+    (9, 5, 5 , 'normal' ,'2024-04-20 11:00:00'::timestamp with time zone),
+    (10, 5, 3 , 'hard' ,'2024-04-20 11:55:00'::timestamp with time zone)
 
 insert into comments (comment) values ('tasts good.')
 
