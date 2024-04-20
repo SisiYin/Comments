4411b4efd2cb2c2efc9c6232f35d39a1fd29292c
SisiYin <t3chyi00@students.oamk.fi>
2024-04-20 13:29:38 +0300

    date

--- server/db.sql
+++ server/db.sql
@@ -9,12 +9,25 @@ CREATE TABLE comments (
 
 insert into comments (comment) values ('tasts good.')
 
+-- 创建触发器函数
+CREATE OR REPLACE FUNCTION update_comment_num()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- 计算帖子的评论数量并更新
+    UPDATE post AS p
+    SET comment_num = (
+        SELECT COUNT(comment)
+        FROM comment AS c
+        WHERE c.post_id = p.post_id
+    )
+    WHERE p.post_id = NEW.post_id;
 
-UPDATE post AS p
-SET comment_num = subquery.comment_num
-FROM (
-    SELECT post_id, COUNT(comment) AS comment_num
-    FROM comments
-    GROUP BY post_id
-) AS subquery
-WHERE p.post_id = subquery.post_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- 创建触发器
+CREATE TRIGGER comment_trigger
+AFTER INSERT OR DELETE ON comment
+FOR EACH ROW
+EXECUTE FUNCTION update_comment_num();
